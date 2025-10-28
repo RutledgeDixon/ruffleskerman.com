1070b0e05a55041ddcc33632cd86ea91be84f86f
Rutledge Dixon <rutledgepdixon@gmail.com>
2025-10-27 20:40:44 -0500

    updated postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-export default {
+// filepath: c:\Users\rutle\source\repos\ruffleskerman.com\postcss.config.js
+module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
-    //autoprefixer: {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
+
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     //autoprefixer: {},
+//   },
+// };
