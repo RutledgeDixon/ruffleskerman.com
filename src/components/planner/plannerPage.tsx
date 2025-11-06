c241a80476a180a8276c6c329a25c103ecec020f
Rutledge Dixon <rutledgepdixon@gmail.com>
2025-11-05 21:33:52 -0600

    added helpful 'category: __' thing below "name's planner"

--- src/components/planner/plannerPage.tsx
+++ src/components/planner/plannerPage.tsx
@@ -71,6 +71,11 @@ export default function PlannerPage() {
                 <h1 className="text-4xl font-bold text-gray-400 my-8 text-center">
                     {userName ? `${userName}'s Planner` : 'RK Planner'}
                 </h1>
+                {userData && userData.categories && userData.categories[currentCategoryIndex] && (
+                    <h2 className="text-2xl font-semibold text-gray-400 mb-4 text-center">
+                        {`Category: ${userData.categories[currentCategoryIndex].title}`}
+                    </h2>
+                )}
                 <div className="planner-cards">
                     {!userData ? (
                         <Login setUserData={handleLogin}/>
